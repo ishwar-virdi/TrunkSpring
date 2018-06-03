a3b1a903fdcee711eebbe46cf8b15de44d5964e8
ishwarvirdi <s3611249@student.rmit.edu.au>
2018-06-03 20:09:17 +1000

    Bug Fixes v2

--- src/main/java/com/trunk/demo/service/Impl/ResultDetailManagerImpl.java
+++ src/main/java/com/trunk/demo/service/Impl/ResultDetailManagerImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.trunk.demo.model.mongo.SettlementStmt;
-import com.trunk.demo.repository.SettlementRepository;
 import com.trunk.demo.service.mongo.ResultDetailManager;
 import com.trunk.demo.vo.ListResultDetailsVO;
 
@@ -30,25 +29,24 @@ public class ResultDetailManagerImpl implements ResultDetailManager {
 	}
 
 	@Override
-	public String markReconcile(JsonArray items){
+	public String markReconcile(JsonArray items) {
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("result","success");
+		jsonObject.addProperty("result", "success");
 		SettlementStmt settle = null;
 		ReconcileResult result = null;
 		String resultsId = null;
 		int count = 0;
-		if(items.size() == 0){
-			jsonObject.addProperty("result","fail");
+		if (items.size() == 0) {
+			jsonObject.addProperty("result", "fail");
 			return jsonObject.toString();
 		}
 
-		for(int i = 0,length = items.size();i < length;i++){
+		for (int i = 0, length = items.size(); i < length; i++) {
 			settle = settlementBO.findByReceiptNumber(items.get(i).getAsLong());
-			if(resultsId == null){
+			if (resultsId == null) {
 				resultsId = settle.getReconcileResultsId();
 			}
-			if(settle != null &&
-					(settle.getReconcileStatus() == 0 || settle.getReconcileStatus() == 1)){
+			if (settle != null) {
 				settle.setReconcileStatus(2);
 				settlementBO.save(settle);
 				count++;
@@ -56,37 +54,36 @@ public class ResultDetailManagerImpl implements ResultDetailManager {
 		}
 
 		result = reconcileResultBO.findById(resultsId);
-		reconcileResultBO.updateIncreaseIsReconcile(count,result);
+		reconcileResultBO.updateIncreaseIsReconcile(count, result);
 		return jsonObject.toString();
 	}
 
 	@Override
-	public String markNotReconcile(JsonArray items){
+	public String markNotReconcile(JsonArray items) {
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("result","success");
+		jsonObject.addProperty("result", "success");
 		SettlementStmt settle = null;
 		ReconcileResult result = null;
 		String resultsId = null;
 		int count = 0;
-		if(items.size() == 0){
-			jsonObject.addProperty("result","fail");
+		if (items.size() == 0) {
+			jsonObject.addProperty("result", "fail");
 			return jsonObject.toString();
 		}
-		for(int i = 0,length = items.size();i < length;i++){
+		for (int i = 0, length = items.size(); i < length; i++) {
 			settle = settlementBO.findByReceiptNumber(items.get(i).getAsLong());
-			if(resultsId == null){
+			if (resultsId == null) {
 				resultsId = settle.getReconcileResultsId();
 			}
-			if(settle != null &&
-					(settle.getReconcileStatus() == 2 || settle.getReconcileStatus() == 3)){
-				settle.setReconcileStatus(0);
+			if (settle != null) {
+				settle.setReconcileStatus(4);
 				settlementBO.save(settle);
 				count++;
 			}
 		}
 
 		result = reconcileResultBO.findById(resultsId);
-		reconcileResultBO.updateIncreaseNotReconcile(count,result);
+		reconcileResultBO.updateIncreaseNotReconcile(count, result);
 		return jsonObject.toString();
 	}
 
